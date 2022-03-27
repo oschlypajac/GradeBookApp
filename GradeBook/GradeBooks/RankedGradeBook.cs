9795088b7f32af96696124070028eca84c99a302
oschlypajac <tomaszapostolski@gmail.com>
2022-03-28 01:28:06 +0200

    d

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,12 +14,58 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students.");
+                throw new InvalidOperationException();
             }
+            //todo
             List<double> averageGrades = new List<double> { averageGrade };
             foreach (var item in Students)
             {
-                averageGrades.Add()
+                averageGrades.Add(item.AverageGrade);
+            }
+            averageGrades.Sort();
+            int index = averageGrades.IndexOf(averageGrade);
+            double num = index / averageGrades.Count;
+            if (num >= 0.8)
+            {
+                return 'A';
+            }
+            else if (num >= 0.6)
+            {
+                return 'B';
+            }
+            else if (num >= 0.4)
+            {
+                return 'C';
+            }
+            else if (num >= 0.2)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }            
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
             }
         }
     }
--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,24 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
-            var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            //todo
+            if (parts[2] == "standard" || parts[2] == "ranked")
+            {
+                var name = parts[2];
+                BaseGradeBook gradeBook = new BaseGradeBook(name);
+                Console.WriteLine("Created gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(gradeBook);
+            }
+            else
+            {
+                Console.WriteLine(parts[2] + " is not supporetd type of gradebook, please try again");
+                return;
+            }
         }
 
         public static void LoadCommand(string command)
