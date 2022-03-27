b33b4ff9e730a204df56822607de55357148fa1f
oschlypajac <tomaszapostolski@gmail.com>
2022-03-27 16:41:07 +0200

    x

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            List<double> averageGrades = new List<double> { averageGrade };
+            foreach (var item in Students)
+            {
+                averageGrades.Add()
+            }
+        }
+    }
+}
